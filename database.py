52f488e5bae1c7426fbb8a3f2939fae63391a484
WEIQIAN17 <32602010+WEIQIAN17@users.noreply.github.com>
2021-05-02 22:41:37 -0500

    Update database.py

--- database.py
+++ database.py
@@ -17,7 +17,7 @@ def get_cypto_price():
 
 ##
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:QianWei@910712@localhost/app'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:*password*/app'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 #db = Marshmallow(app)
